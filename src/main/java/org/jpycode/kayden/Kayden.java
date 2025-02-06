53f8e4d5a6a49ac52ff18be7cf495092689c325d
JPyCode <marcosrobertosilva.contato@gmail.com>
2025-02-06 20:03:47 -0300

    .

--- src/main/java/org/jpycode/kayden/Kayden.java
+++ src/main/java/org/jpycode/kayden/Kayden.java
@@ -6,26 +6,31 @@ import org.jpycode.kayden.commands.OpenStatusGUICommand;
 import org.jpycode.kayden.commands.OpenSwordsGUICommand;
 import org.jpycode.kayden.gui.statusGUI.StatusGUI;
 import org.jpycode.kayden.gui.swordsGUI.SwordsGUI;
-import org.jpycode.kayden.listeners.DoubleJumpListener;
+import org.jpycode.kayden.listeners.DamageTracker;
+import org.jpycode.kayden.listeners.KillListener;
 import org.jpycode.kayden.listeners.MentionChatListener;
 import org.jpycode.kayden.listeners.ThunderSwordListener;
-import org.jpycode.kayden.scoreboard.Board;
+import org.jpycode.kayden.scoreboard.MainScoreboard;
+
+import java.util.HashMap;
 
 public final class Kayden extends JavaPlugin {
-    private BukkitTask task;
+    private MainScoreboard mainScoreboard;
+    private final HashMap<String, Integer> playerKills = new HashMap<>();
 
     @Override
     public void onEnable() {
         getLogger().info("@ Kayden Plugin enabled");
-        /* Scoreboard */
-        task = getServer().getScheduler().runTaskTimer(this, Board.getInstance(), 0, 20);
+        mainScoreboard = new MainScoreboard(this, playerKills);
+
 
         /* Listeners */
         getServer().getPluginManager().registerEvents(new SwordsGUI(this), this);
         getServer().getPluginManager().registerEvents(new ThunderSwordListener(), this);
-        getServer().getPluginManager().registerEvents(new DoubleJumpListener(), this);
         getServer().getPluginManager().registerEvents(new StatusGUI(this), this);
         getServer().getPluginManager().registerEvents(new MentionChatListener(), this);
+        getServer().getPluginManager().registerEvents(new KillListener(playerKills, mainScoreboard), this);
+        getServer().getPluginManager().registerEvents(new DamageTracker(this), this);
 
         /* Commands */
         getCommand("swords").setExecutor(new OpenSwordsGUICommand(new SwordsGUI(this)));
