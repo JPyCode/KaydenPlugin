56c939f5106cd2b1fe5a9d0544e6b92b2d583d9b
JPyCode <marcosrobertosilva.contato@gmail.com>
2025-02-06 22:25:56 -0300

    feat: created a economy system. The next step is to integrate it with a database to avoid relying on YAML.

--- src/main/java/org/jpycode/kayden/Kayden.java
+++ src/main/java/org/jpycode/kayden/Kayden.java
@@ -1,6 +1,9 @@
 package org.jpycode.kayden;
 
+import lombok.Getter;
+import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jpycode.kayden.commands.economy.MoneyCommand;
 import org.jpycode.kayden.commands.status.OpenStatusGUICommand;
 import org.jpycode.kayden.commands.swords.OpenSwordsGUICommand;
 import org.jpycode.kayden.gui.statusGUI.StatusGUI;
@@ -11,14 +14,19 @@ import org.jpycode.kayden.listeners.MentionChatListener;
 import org.jpycode.kayden.listeners.ThunderSwordListener;
 import org.jpycode.kayden.scoreboard.MainScoreboard;
 
+import java.io.File;
 import java.util.HashMap;
 
 public final class Kayden extends JavaPlugin {
     private MainScoreboard mainScoreboard;
     private final HashMap<String, Integer> playerKills = new HashMap<>();
+    @Getter
+    private static Kayden instance;
+
 
     @Override
     public void onEnable() {
+        instance = this;
         getLogger().info("@ Kayden Plugin enabled");
         mainScoreboard = new MainScoreboard(this, playerKills);
 
@@ -34,6 +42,7 @@ public final class Kayden extends JavaPlugin {
         /* Commands */
         getCommand("swords").setExecutor(new OpenSwordsGUICommand(new SwordsGUI(this)));
         getCommand("status").setExecutor(new OpenStatusGUICommand(new StatusGUI(this)));
+        getCommand("money").setExecutor(new MoneyCommand());
 
     }
 
---
+++ src/main/java/org/jpycode/kayden/commands/economy/MoneyCommand.java
@@ -0,0 +1,129 @@
+package org.jpycode.kayden.commands.economy;
+
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
+import org.bukkit.entity.Player;
+import org.jetbrains.annotations.NotNull;
+import org.jpycode.kayden.Kayden;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+public class MoneyCommand implements CommandExecutor {
+    private final File economyFile;
+    private final FileConfiguration economyConfig;
+
+    public MoneyCommand() {
+        this.economyFile = new File(Kayden.getInstance().getDataFolder(), "economy.yml");
+        this.economyConfig = YamlConfiguration.loadConfiguration(economyFile);
+    }
+
+    public double getBalance(UUID playerID) {
+        return economyConfig.getDouble(playerID.toString(), 0.0);
+    }
+
+    public void setBalance(UUID playerID, double value) {
+        economyConfig.set(playerID.toString(), value);
+        saveConfig();
+    }
+
+    private void saveConfig() {
+        try {
+            economyConfig.save(economyFile);
+        } catch (IOException e) {
+            Bukkit.getLogger().severe("Error at saving file economy.yml");
+        }
+    }
+
+
+    @Override
+    public boolean onCommand(@NotNull CommandSender sender, @NotNull Command command, @NotNull String label, @NotNull String @NotNull [] args) {
+//        if (!(sender instanceof Player)) {
+//            sender.sendMessage("Only players can use this command.");
+//            return true;
+//        }
+
+        Player player = (Player) sender;
+
+        switch (args.length) {
+            case 0:
+                player.sendMessage(String.format(ChatColor.GREEN + "Your balance: " + ChatColor.GOLD + "$ %.2f", getBalance(player.getUniqueId())));
+                return true;
+
+            case 3:
+                Player target = Bukkit.getPlayer(args[1]);
+
+                if((args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("remove")) && !player.isOp()) {
+                    player.sendMessage(ChatColor.RED + "You do not have permission to execute that command.");
+                    return true;
+                }
+
+                if (target == null) {
+                    player.sendMessage(ChatColor.RED + "Player not found.");
+                    return true;
+                }
+                double value;
+
+                try {
+                    value = Double.parseDouble(args[2]);
+                } catch (NumberFormatException e) {
+                    player.sendMessage(ChatColor.RED + "Type a valid value.");
+                    return true;
+                }
+
+                switch (args[0].toLowerCase()) {
+                    case "add":
+                        systemAddBalance(player, target, value, true);
+                        return true;
+
+
+                    case "remove":
+                        systemRemoveBalance(player, target, value, false);
+                        return true;
+
+                    case "pay":
+                        if (getBalance(player.getUniqueId()) < value) {
+                            player.sendMessage(ChatColor.RED + "Insufficient balance");
+                            return true;
+                        }
+
+                        if(player.getUniqueId() == target.getUniqueId()) {
+                            player.sendMessage(ChatColor.RED + "You can't send money to yourself.");
+                            return true;
+                        }
+
+                        setBalance(player.getUniqueId(), getBalance(player.getUniqueId()) - value);
+                        setBalance(target.getUniqueId(), getBalance(target.getUniqueId()) + value);
+
+                        player.sendMessage(ChatColor.GREEN + "You sent $ " + value + " to " + target.getName());
+                        target.sendMessage(ChatColor.GOLD + "You received " + value + " from " + player.getName());
+                        return true;
+                }
+
+            default:
+                if(player.isOp()) {
+                    player.sendMessage("Use: /money [add/remove/pay] <nick> <quantity>");
+                } else player.sendMessage("Use: /money pay <nick> <quantity>");
+
+                return true;
+        }
+    }
+
+    public void systemAddBalance(Player player, Player target, double value, boolean message) {
+        setBalance(target.getUniqueId(), getBalance(target.getUniqueId()) + value);
+        player.sendMessage(ChatColor.GREEN + "You added $ " + value + " to " + target.getName());
+        if(message) target.sendMessage(ChatColor.GOLD + "You received $ " + value + " from system.");
+    }
+
+    public void systemRemoveBalance(Player player, Player target, double value, boolean message) {
+        setBalance(target.getUniqueId(), getBalance(target.getUniqueId()) - value);
+        player.sendMessage(ChatColor.RED + "You removed $ " + value + " from " + target.getName());
+        if(message) target.sendMessage(ChatColor.RED + "$ " + value + " has been taken of your balance");
+    }
+}
