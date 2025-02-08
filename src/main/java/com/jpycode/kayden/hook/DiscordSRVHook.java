9e7a7873ce6b2383831be0f980542f2dd4a7f90b
JPyCode <marcosrobertosilva.contato@gmail.com>
2025-02-08 14:54:08 -0300

    adding discord srv

--- src/main/java/com/jpycode/kayden/hook/DiscordSRVHook.java
+++ src/main/java/com/jpycode/kayden/hook/DiscordSRVHook.java
@@ -1,12 +1,28 @@
 package com.jpycode.kayden.hook;
 
 import github.scarsz.discordsrv.DiscordSRV;
+import github.scarsz.discordsrv.api.Subscribe;
+import github.scarsz.discordsrv.api.events.DiscordGuildMessageReceivedEvent;
+import org.bukkit.Bukkit;
+import org.bukkit.ChatColor;
 
 public class DiscordSRVHook {
     private static final DiscordSRVHook instance = new DiscordSRVHook();
 
     private DiscordSRVHook() {
 
+    }
+
+    @Subscribe
+    public void onMessageReceived(DiscordGuildMessageReceivedEvent event) {
+        String playerName = event.getMember().getEffectiveName();
+        String channel = event.getChannel().getName();
+        String message = event.getMessage().getContentRaw();
+
+        Bukkit.broadcastMessage(ChatColor.translateAlternateColorCodes(
+                '&', "&7[&bDiscord&7] &f" + playerName
+                + " &7in &f" + channel + " &8>> &f" + message
+        ));
     }
     public static void register() {
         DiscordSRV.api.subscribe(instance);
