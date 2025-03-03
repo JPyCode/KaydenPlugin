fb7c089c1f6619944a4559ec19b06e5c0b8e8a8c
JPyCode <marcosrobertosilva.contato@gmail.com>
2025-03-03 16:00:14 -0300

    -

--- src/main/java/com/jpycode/kayden/Kayden.java
+++ src/main/java/com/jpycode/kayden/Kayden.java
@@ -1,73 +1,64 @@
 package com.jpycode.kayden;
 
-import com.jpycode.kayden.hook.DiscordSRVHook;
-import com.jpycode.kayden.listeners.market.MainMenuListener;
+import com.jpycode.kayden.economy.commands.SwordShop;
+import com.jpycode.kayden.economy.market.MarketSystem;
+import com.jpycode.kayden.scoreboard.MainScoreboard;
 import lombok.Getter;
+import org.bukkit.Bukkit;
+import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import com.jpycode.kayden.commands.crate.CrateCommand;
-import com.jpycode.kayden.commands.economy.Eco;
-import com.jpycode.kayden.commands.economy.MarketCommand;
-import com.jpycode.kayden.commands.economy.Money;
-import com.jpycode.kayden.commands.economy.Pay;
 import com.jpycode.kayden.commands.status.OpenStatusGUICommand;
 import com.jpycode.kayden.commands.swords.OpenSwordsGUICommand;
 import com.jpycode.kayden.database.Database;
-import com.jpycode.kayden.gui.marketGUI.MarketMenu;
 import com.jpycode.kayden.gui.statusGUI.StatusGUI;
 import com.jpycode.kayden.gui.swordsGUI.SwordsGUI;
 import com.jpycode.kayden.listeners.KillListener;
 import com.jpycode.kayden.listeners.ChatListener;
-import com.jpycode.kayden.listeners.PlayerJoinListener;
 import com.jpycode.kayden.listeners.ThunderSwordListener;
-import com.jpycode.kayden.managers.CrateManager;
-import com.jpycode.kayden.scoreboard.MainScoreboard;
+import com.jpycode.kayden.economy.commands.economy.*;
+
+import java.io.File;
+
 
 public final class Kayden extends JavaPlugin {
-    private MainScoreboard mainScoreboard;
     @Getter
     private static Kayden instance;
-    private final Database database = new Database(this);
-    private CrateManager crateManager;
-
+    private MainScoreboard scoreboard;
 
     @Override
     public void onEnable() {
-        database.connect()
-                .thenRun(() -> getLogger().info("Conexão com o banco de dados estabelecida com sucesso!"))
-                .exceptionally(ex -> {
-                    getLogger().severe("Erro: " + ex.getMessage());
-                    return null;
-                });
         instance = this;
-        mainScoreboard = new MainScoreboard(this);
+        Database.connect();
 
+        saveDefaultFile("config.yml");
+        saveDefaultFile("shop.yml");
+
+        this.scoreboard = new MainScoreboard(this);
+        for (Player player : Bukkit.getOnlinePlayers()) {
+            scoreboard.updateScoreboard(player);
+        }
 
 
         /* Listeners */
         getServer().getPluginManager().registerEvents(new SwordsGUI(this), this);
         getServer().getPluginManager().registerEvents(new ThunderSwordListener(), this);
-        getServer().getPluginManager().registerEvents(new StatusGUI(this), this);
+        getServer().getPluginManager().registerEvents(new StatusGUI(), this);
         getServer().getPluginManager().registerEvents(new ChatListener(), this);
-        getServer().getPluginManager().registerEvents(new KillListener(mainScoreboard, this), this);
-        getServer().getPluginManager().registerEvents(new PlayerJoinListener(), this);
-        getServer().getPluginManager().registerEvents(new MainMenuListener(), this);
+        getServer().getPluginManager().registerEvents(new KillListener(), this);
+
 
 
         /* Commands */
         getCommand("swords").setExecutor(new OpenSwordsGUICommand(new SwordsGUI(this)));
-        getCommand("status").setExecutor(new OpenStatusGUICommand(new StatusGUI(this)));
+        getCommand("status").setExecutor(new OpenStatusGUICommand(new StatusGUI()));
+
+        // Economy
         getCommand("money").setExecutor(new Money());
         getCommand("pay").setExecutor(new Pay());
         getCommand("eco").setExecutor(new Eco());
-        getCommand("crates").setExecutor(new CrateCommand(crateManager));
-        getCommand("market").setExecutor(new MarketCommand(new MarketMenu()));
+        getCommand("market").setExecutor(new MarketSystem());
+        getCommand("shop").setExecutor(new SwordShop());
 
-
-
-
-
-        if(getServer().getPluginManager().getPlugin("DiscordSRV") != null)
-            DiscordSRVHook.register();
 
 
         getLogger().info("@ Kayden Plugin enabled");
@@ -75,12 +66,19 @@ public final class Kayden extends JavaPlugin {
 
     @Override
     public void onDisable() {
-        if(getServer().getPluginManager().getPlugin("DiscordSRV") != null)
-            DiscordSRVHook.unregister();
 
-
+        Database.close();
 
         getLogger().info("@ Kayden Plugin disabled");
+    }
+
+    private void saveDefaultFile(String fileName) {
+        File file = new File(getDataFolder(), fileName);
+
+        if (!file.exists()) {
+            getLogger().info(fileName + " não encontrado! Criando...");
+            saveResource(fileName, false);
+        }
     }
 
 }
