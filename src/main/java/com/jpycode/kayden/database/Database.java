3b39d1a39323fc65860812332522165322fb59ea
JPyCode <marcosrobertosilva.contato@gmail.com>
2025-03-03 15:58:37 -0300

    fixing database

--- src/main/java/com/jpycode/kayden/database/Database.java
+++ src/main/java/com/jpycode/kayden/database/Database.java
@@ -1,18 +1,20 @@
 package com.jpycode.kayden.database;
 
+import com.jpycode.kayden.Kayden;
+import lombok.Getter;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.sql.*;
 import java.util.concurrent.CompletableFuture;
 
-public class Database {
-    private static JavaPlugin plugin;
-    private static Connection connection;
+import static com.jpycode.kayden.scoreboard.MainScoreboard.updateMoneyInCache;
 
-    public Database(JavaPlugin plugin) {
-        Database.plugin = plugin;
-    }
+public class Database {
+    @Getter
+    private static final JavaPlugin plugin = Kayden.getInstance();
+    @Getter
+    public static Connection connection;
 
     public static CompletableFuture<Void> connect() {
         return CompletableFuture.runAsync(() -> {
@@ -26,51 +28,14 @@ public class Database {
                 String database = plugin.getConfig().getString("database.database");
                 String username = plugin.getConfig().getString("database.username");
                 String password = plugin.getConfig().getString("database.password");
-
                 String url = "jdbc:mysql://" + host + ":" + port + "/" + database + "?useSSL=false&autoReconnect=true";
                 connection = DriverManager.getConnection(url, username, password);
-                plugin.getLogger().info("Conexão com MySQL estabelecida!");
+                plugin.getLogger().info("Conexão com MySQL estabelecida <" + database + ">!");
             } catch (SQLException | ClassNotFoundException e) {
                 plugin.getLogger().severe("Erro ao conectar ao banco de dados: " + e.getMessage());
                 throw new RuntimeException(e);
             }
         });
-    }
-
-    public static void close() {
-        try {
-            if(connection != null && !connection.isClosed()) {
-                connection.close();
-                System.out.println("[Kayden] Connection with MySql closed.");
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-    }
-
-    public static Connection getConnection() {
-        try{
-            if(connection == null || connection.isClosed()) {
-                connect();
-            }
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
-        return connection;
-    }
-
-    public static void savePlayerData(Player player) {
-        String query = "INSERT INTO players_table (uuid, name, kills) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE name = ?";
-
-        try(PreparedStatement stmt = Database.getConnection().prepareStatement(query)) {
-            stmt.setString(1, player.getUniqueId().toString());
-            stmt.setString(2, player.getName());
-            stmt.setInt(3, 0);
-            stmt.setString(4, player.getName());
-            stmt.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
     }
 
     public static CompletableFuture<Double> getBalance(Player player) {
@@ -97,15 +62,50 @@ public class Database {
                 if (connection == null || connection.isClosed()) {
                     connect().join();
                 }
-                try (PreparedStatement stmt = connection.prepareStatement("INSERT INTO economy (uuid, balance) VALUES (?, ?) ON DUPLICATE KEY UPDATE balance = ?")) {
+                try (PreparedStatement stmt = connection.prepareStatement("INSERT INTO economy (uuid, name, balance) VALUES (?, ?, ?) ON DUPLICATE KEY UPDATE name = ?, balance = ?")) {
                     stmt.setString(1, player.getUniqueId().toString());
-                    stmt.setDouble(2, value);
+                    stmt.setString(2, player.getName());
                     stmt.setDouble(3, value);
+                    stmt.setString(4, player.getName());
+                    stmt.setDouble(5, value);
                     stmt.executeUpdate();
+                    updateMoneyInCache(player, value);
                 }
             } catch (SQLException e) {
                 plugin.getLogger().severe("Erro ao definir saldo de " + player.getName() + ": " + e.getMessage());
                 throw new RuntimeException(e);
+            }
+        });
+    }
+
+    public static void close() {
+        try {
+            if (connection != null && !connection.isClosed()) {
+                connection.close();
+                plugin.getLogger().info("Conexão com MySQL encerrada.");
+            }
+        } catch (SQLException e) {
+            plugin.getLogger().severe("Erro ao fechar conexão com MySQL: " + e.getMessage());
+        }
+    }
+
+    public static CompletableFuture<Integer> getKills(Player player) {
+        return CompletableFuture.supplyAsync(() -> {
+            String query = "SELECT kills FROM players_table WHERE uuid = ?";
+            try (PreparedStatement stmt = getConnection().prepareStatement(query)) {
+
+                stmt.setString(1, player.getUniqueId().toString());
+                ResultSet rs = stmt.executeQuery();
+                if(rs.next()) {
+                    return rs.getInt("kills");
+                } else {
+                    System.out.println("Nenhum dado encontrado para " + player.getName());
+                    return 0; // Retorna 0 se o jogador ainda não tiver registro
+                }
+
+            } catch (SQLException e) {
+                plugin.getLogger().severe("Erro ao obter kills de " + player.getName() + ": " + e.getMessage());
+                return 0;
             }
         });
     }
